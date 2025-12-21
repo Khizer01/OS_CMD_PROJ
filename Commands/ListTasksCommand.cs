17abd4beeec10338fb1bdf8b02eaeae1dc0d9b92
Mishal Khalid <mishalkhalid445@gmail.com>
2025-12-21 22:48:44 +0500

    Update ListTasksCommand.cs

--- Commands/ListTasksCommand.cs
+++ Commands/ListTasksCommand.cs
@@ -93,40 +93,6 @@ namespace OS_CMD_PROJECT.Commands
                 Console.WriteLine("Invalid PID.");
             }
 
-                Console.SetCursorPosition(0, 0);
-
-                Console.WriteLine(" ID     Process Name               Memory (MB)");
-                Console.WriteLine("-----------------------------------------------");
-
-                var processes = Process.GetProcesses();
-
-                foreach (var process in processes)
-                {
-                    try
-                    {
-                        string name = process.ProcessName;
-
-                        if (name.Length > 25)
-                            name = name.Substring(0, 22) + "...";
-
-                        double memoryMb = process.WorkingSet64 / (1024.0 * 1024.0);
-
-                        Console.WriteLine(
-                            $"{process.Id,-6} {name,-25} {memoryMb,10:F2}"
-                        );
-                    }
-                    catch
-                    {
-                        // Ignore inaccessible system processes
-                    }
-                }
-
-                Console.WriteLine("\nPress Q or ESC to exit...");
-                Thread.Sleep(3000);
-            }
-
-            Console.CursorVisible = true;
-            Console.Clear();
             return Task.CompletedTask;
         }
     }
